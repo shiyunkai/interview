034bff0eea90f8c4c8f5f90ff7d0914173498fb5
shiyunkai <2832691898@qq.com>
2019-06-20 17:40:01 +0800

    update

--- src/com/interview/adv/Solution.java
+++ src/com/interview/adv/Solution.java
@@ -1,76 +1,153 @@
 package com.interview.adv;
 
+import java.util.Stack;
+
 /**
  * @Auther: shiyunkai
  * @Date: 2019/06/18 15:39
  * @Description:
  */
 public class Solution {
+
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
-        if(pre.length == 0 || in.length==0){
+        return reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+    public int FibonacciRecursion(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+    }
+
+    public int FibonacciLoop(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 1, b = 1, c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+
+    public class MyQueue {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+        public void push(int node) {
+            stack1.push(node);
+        }
+
+        public int pop() {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            int top = stack2.pop();
+            while (!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+            return top;
+        }
+    }
+
+
+    /**
+     * @param pre      前序遍历
+     * @param startPre 前序遍历开始索引
+     * @param endPre   前序遍历结束索引
+     * @param in       中序遍历
+     * @param startIn  中序遍历开始索引
+     * @param endIn    中序遍历结束索引
+     * @return
+     */
+    public TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+
+        if (startPre > endPre || startIn > endIn) {
             return null;
         }
-        // 根元素
-        int rootVal = pre[0];
-        // 找到中序遍历中的根结点位置
-        int rootIndex = findInRootIndex(in, rootVal);
-        // 分别找到左右子数的前序、中序遍历
-        TreeNode rootNode = new TreeNode(rootVal);
-        rootNode.left = reConstructBinaryTree(findPreOrder(rootIndex, pre, 0), findInOrder(rootIndex, in, 0));
-        rootNode.right = reConstructBinaryTree(findPreOrder(rootIndex, pre, 1), findInOrder(rootIndex, in, 1));
+
+        TreeNode rootNode = new TreeNode(pre[startPre]);
+        // 找到根元素在中序遍历中的索引
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                // 找到了，构建二叉树
+                rootNode.left = reConstructBinaryTree(pre, startPre + 1, i - startIn + startPre, in, startIn, i - 1);
+                rootNode.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+                break;
+            }
+        }
         return rootNode;
     }
 
-    public int findInRootIndex(int[] in, int rootVal) {
-        for (int i = 0; i < in.length; i++) {
-            if (in[i] == rootVal) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    /*
-     * type:0 查找的是左子数的中序遍历，否则为右子数的中序遍历
-     */
-    public int[] findInOrder(int rootIndex, int[] in, int type) {
-        int[] childIn;
-        if (type == 0) {
-            //child left
-            childIn = new int[rootIndex];
-            for (int i = 0; i < rootIndex; i++) {
-                childIn[i] = in[i];
+    // 计算整数的二进制中1的个数，负数用补码表示
+    public int NumberOf1(int n) {
+        int count = 0;
+        if (n >= 0) {
+            while (n >> 1 != 0 || n % 2 == 1) {
+                if (n % 2 == 1) {
+                    count++;
+                }
+                n = n >> 1;
             }
         } else {
-            //child right
-            childIn = new int[in.length - rootIndex - 1];
-            for (int i = rootIndex + 1; i < in.length; i++) {
-                childIn[i] = in[i];
+            // 计算补码
+            for (int i = 0; i < 32; i++) {
+                int t = (n & 0x80000000 >>> i) >>> (31 - i);
+                if (t % 2 == 1) {
+                    count++;
+                }
             }
         }
-        return childIn;
-
+        return count;
     }
 
-    /*
-     * type:0 查找的是左子数的前序遍历，否则为右子数的前序遍历
-     */
-    public int[] findPreOrder(int rootIndex, int[] pre, int type) {
-        int[] childPre;
-        if (type == 0) {
-            childPre = new int[rootIndex];
-            for (int i = 1; i <= rootIndex; i++) {
-                childPre[i-1] = pre[i];
-            }
-        } else {
-            childPre = new int[pre.length - rootIndex - 1];
-            for (int i = rootIndex + 1; i <= pre.length; i++) {
-                childPre[i-rootIndex-1] = pre[i];
+    public int NumberOf2(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    public int NumberOf3(int n) {
+
+        int count = 0;
+        String nStr = Integer.toBinaryString(n);
+        for (int i = 0; i < nStr.length(); i++) {
+            if (nStr.charAt(i) == '1') {
+                count++;
             }
         }
-        return childPre;
+        return count;
     }
+
+    // 求base的 exponent次方
+    public double Power(double base, int exponent) {
+        if (base <= 0 || base == 1) {
+            throw new RuntimeException("基数必须大于0，且不等于1");
+        }
+        if (exponent == 0) {
+            return 1d;
+        }
+        double result = base;
+        int j = (exponent < 0) ? -exponent : exponent;
+        for (int i = 1; i < j; i++) {
+            result *= base;
+        }
+
+        return exponent > 0 ? result : 1 / result;
+    }
+
+
 }
-
-
-
--- src/com/interview/adv/Test.java
+++ src/com/interview/adv/Test.java
@@ -8,6 +8,31 @@ package com.interview.adv;
 public class Test {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        TreeNode treeNode = solution.reConstructBinaryTree(new int[]{1, 2, 4, 7, 3, 5, 6, 8}, new int[]{4, 7, 2, 1, 5, 3, 8, 6});
+        //solution.reConstructBinaryTree(new int[]{1, 2, 4, 7, 3, 5, 6, 8}, new int[]{4, 7, 2, 1, 5, 3, 8, 6});
+        //Solution.MyQueue myQueue = solution.new MyQueue();
+        //myQueue.push(1);
+        //myQueue.push(2);
+        //myQueue.pop();
+        //myQueue.push(3);
+        //myQueue.push(4);
+        //myQueue.pop();
+        //long start = System.currentTimeMillis();
+        //int result = solution.FibonacciRecursion(100);
+        //long time = (System.currentTimeMillis()-start)/1000;
+        //System.out.println(String.format("result:%d FibonacciRecursion:%d秒",result,time));
+        //start = System.currentTimeMillis();
+        //result = solution.FibonacciLoop(100);
+        //time = (System.currentTimeMillis()-start)/1000;
+        //System.out.println(String.format("result:%d FibonacciLoop:%d秒",result,time));
+
+        //int count1 = solution.NumberOf1(-1);
+        //System.out.println("二进制中1的个数为："+count1);
+        //int count2 = solution.NumberOf2(-1);
+        //System.out.println("二进制中1的个数为："+count2);
+        //int count3 = solution.NumberOf3(7);
+        //System.out.println("二进制中1的个数为："+count3);
+
+        double power = solution.Power(3.0, -2);
+        System.out.println(power);
     }
 }
